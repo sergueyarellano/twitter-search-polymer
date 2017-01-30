70a42b480cd2fb95622ff4c53df325901686f083
Serguey Arellano <serguey.arellano.contractor@bbva.com>
2017-01-30 19:40:09 +0100

    update service worker options

--- swconfig/sw-precache-config.js
+++ swconfig/sw-precache-config.js
@@ -5,7 +5,16 @@ module.exports = {
   ],
   stripPrefix: 'src/',
   runtimeCaching: [{
-    urlPattern: /^http:/,
+    urlPattern: /^http:\/\/127\.0\.0\.1:3000\/api/,
     handler: 'networkFirst'
+  },{
+    urlPattern: /^\?q=/,
+    handler: 'fastest',
+    options: {
+        cache: {
+          maxEntries: 10,
+          name: 'req-cache'
+        }
+    }
   }]
 };
